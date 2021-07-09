1e19b01a2693bcefd94fc08bc4f086065adbbbbd
Volodymyr Batsyk <vovabatsyk.lviv@gmail.com>
2021-07-09 14:51:43 +0300

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+export const Navbar = () => {
+	const [active, setActive] = useState('About')
+
+	return (
+		<div className='navbar'>
+			<div className='navbar__active'>{active}</div>
+			<div className='navbar__items'>
+				{active !== 'About' && (
+					<div
+						className='navbar__item'
+						onClick={() => setActive('About')}
+					>
+						About
+					</div>
+				)}
+
+				{active !== 'Resume' && (
+					<div
+						className='navbar__item'
+						onClick={() => setActive('Resume')}
+					>
+						Resume
+					</div>
+				)}
+				{active !== 'Projects' && (
+					<div
+						className='navbar__item'
+						onClick={() => setActive('Projects')}
+					>
+						Projects
+					</div>
+				)}
+			</div>
+		</div>
+	)
+}
