68a2d7ac262c6f86bb77572d64a7d78b9243ff7f
Volodymyr Batsyk <vovabatsyk.lviv@gmail.com>
2021-07-18 21:17:57 +0300

    Bug fixed (empty chars)

--- src/components/About.js
+++ src/components/About.js
@@ -4,21 +4,21 @@ import { skills } from '../data/data'
 import { SkillCard } from './SkillCard'
 
 export const About = () => {
-        return ( <div className = 'about'>
-                    <h6 className = 'about__intro' >
-                        Result - oriented, highly motivated Software Developer.Proficient both in Frontend( < code > JavaScript < /code>) andBackend technologies( < code > C# < /code>). 
-                            </h6>
-                    <div className = 'container about__container' >
-                        <h6 className = 'about__heading' > What I offer </h6>
-                        <div className = 'row' > {
-                            skills.map(skill => ( <
-                                SkillCard skill = { skill }
-                                key = { generateKey(skill.title) }
-                                />
-                            ))
-                        } 
-                        </div> 
-                    </div>
-                </div>
-                )
-                }
+	return (
+		<div className='about'>
+			<h6 className='about__intro'>
+				Result - oriented, highly motivated Software
+				Developer.Proficient both in Frontend (<code>JavaScript</code>) 
+				and Backend technologies (<code>C#</code>).
+			</h6>
+			<div className='container about__container'>
+				<h6 className='about__heading'> What I offer </h6>
+				<div className='row'>
+					{skills.map(skill => (
+						<SkillCard skill={skill} key={generateKey(skill.title)} />
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
